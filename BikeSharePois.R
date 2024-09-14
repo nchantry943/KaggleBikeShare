1faa47e01df4d2de0209b83f5c7f685ec89193f0
Nathan Chantry <nathantchantry@gmail.com>
2024-09-14 12:21:28 -0600

    Added Poisson Regression with the same model

---
+++ BikeSharePois.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(tidymodels)
+library(vroom)
+library(poissonreg)
+
+## Upload Data
+train <- vroom('train.csv')
+test <- vroom('test.csv')
+
+## Create Poisson Model
+pois_mod <- poisson_reg() |>
+  set_engine('glm') |>
+  set_mode("regression") |>
+  fit(count ~ factor(season) + factor(holiday) + factor(workingday) + factor(weather) + temp + humidity + windspeed, data = train)
+
+bike_pois_pred <- predict(pois_mod, new_data = test)
+
+pois_sub <- bike_pois_pred |>
+  bind_cols(test) |>
+  select(datetime, .pred) |>
+  rename(count = .pred) |>
+  mutate(datetime = as.character(format(datetime)))
+
+vroom_write(x=pois_sub, file = "./PoisPreds.csv", delim=",")
